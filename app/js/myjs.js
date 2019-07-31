79e335e62ab2b762e2ae4f428f82273efa4de4c1
ITkah <ilatkacenko132@gmail.com>
2019-07-31 23:11:28 +0300

    сделал каталог

--- app/js/myjs.js
+++ app/js/myjs.js
@@ -25,5 +25,43 @@ $(document).ready(function() {
 
     $(".owl-carousel").owlCarousel();
 
+    $(".sort-disabled").on("click", function(e){
+        e.preventDefault();
+        $(".sort-disabled").removeClass("sort-active");
+        $(this).addClass("sort-active");
+    });
+
+    $(".mob-katalog").on("click", function(e){
+        e.preventDefault();
+        $(".katalog-link, .sort").toggle(100);
+    });
+
+    
+	var navFixed = $(".header"),
+    navHeight = $('.header').height();
+
+    if ($(window).scrollTop()) {
+        navFixed.addClass("fixed").removeClass("nofixed");
+    }
+
+    if (navFixed.length) {
+        $(window).scroll(function(){
+            var scroll = $(window).scrollTop();
+            
+            if (scroll < navHeight + 50) {
+                navFixed.addClass("nofixed");
+                navFixed.removeClass("fixed hidden");
+            }
+            if (scroll > navHeight){
+                navFixed.addClass("hidden");
+            }
+            if (scroll > navHeight + 50){
+                navFixed.addClass("fixed");
+                navFixed.removeClass("nofixed");
+            }
+            
+        });
+    }
+
 });
 
